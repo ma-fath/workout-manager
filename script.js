d87519995fdaf0e51ba862e9f2aee40c654ee198
Maximilien Fathi <ma_fath@live.concordia.ca>
2024-02-12 21:55:05 -0500

    Integrate Leaflet library for interactive map visualization

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+let map, mapEvent;
+
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(function (position) {
+        // PART 1 - MAIN VARIABLES
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+        const coords = [latitude, longitude];
+        map = L.map("map").setView(coords, 13);
+
+        // PART 2 - TILE LAYER
+        L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+            attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
+        }).addTo(map);
+    }, function () {
+        alert("Could not get your position :(");
+    });
