6f61ee1b7c1a20453ddd385aa9c9e3aaea188657
Maximilien Fathi <ma_fath@live.concordia.ca>
2024-02-13 14:01:59 -0500

    Introduce class and methods for cleaner code structure

--- script.js
+++ script.js
@@ -10,55 +10,79 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-let map, mapEvent;
+class App {
+    // Private instance properties
+    #map;
+    #mapEvent;
 
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(function (position) {
-        // PART 1 - MAIN VARIABLES
+    // Method 1 - Constructor
+    constructor() {
+        this._getPosition();
+        // An event handler function will always have the "this" keyword of the DOM element onto which it is attached.
+        // So below, "this" points to the form not the app object!
+        // So to fix this, must use bind method.
+        form.addEventListener("submit", this._newWorkout.bind(this));
+        inputType.addEventListener("change", this._toggleElevationField.bind(this));
+    }
+
+    // Method 2
+    _getPosition() {
+        if (navigator.geolocation)
+            // Must use bind method else "this" will be set to undefined!
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                alert("Could not get your position :(");
+            });
+    }
+
+    // Method 3
+    _loadMap(position) {
         const {latitude} = position.coords;
         const {longitude} = position.coords;
         const coords = [latitude, longitude];
-        map = L.map("map").setView(coords, 13);
+        // "this" is undefined below if bind method not used when calling _loadMap()
+        this.#map = L.map("map").setView(coords, 13);
 
-        // PART 2 - TILE LAYER
         L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
             attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
-        }).addTo(map);
+        }).addTo(this.#map);
 
-        // PART 3 - HANDLING CLICKS ON MAP
-        map.on("click", function (mapE) {
-            mapEvent = mapE;
-            form.classList.remove("hidden");
-            inputDistance.focus();
-        });
-    }, function () {
-        alert("Could not get your position :(");
-    });
+        // Handling clicks on map
+        this.#map.on("click", this._showForm.bind(this));
+    }
 
-// PART 4 -
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
+    // Method 4
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+    }
 
-    // Clear input fields
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+    // Method 5
+    _toggleElevationField() {
+        inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+        inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    }
 
-    // Displaying marker
-    console.log(mapEvent);
-    const {lat, lng} = mapEvent.latlng;
-    L.marker([lat, lng]).addTo(map)
-        .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: "running-popup"
-        }))
-        .setPopupContent("Workout")
-        .openPopup();
-});
+    // Method 6
+    _newWorkout(e) {
+        e.preventDefault();
 
-// part 5 -
-inputType.addEventListener("change", function () {
-    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+        // Clear input fields
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+
+        // Displaying marker
+        const {lat, lng} = this.#mapEvent.latlng;
+        L.marker([lat, lng]).addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: "running-popup"
+            }))
+            .setPopupContent("Workout")
+            .openPopup();
+    }
+}
+
+const app = new App();
